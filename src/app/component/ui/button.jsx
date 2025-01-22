126df0281c70b16a638781b89aa6fd94aebaed63
Rafi Chandra <crafi2606@gmail.com>
2025-01-22 14:03:15 +0700

    Added hover button on navbar

--- src/app/component/ui/button.jsx
+++ src/app/component/ui/button.jsx
@@ -1,13 +1,13 @@
 export function ButtonWhite({ children }) {
   return (
-    <button className="bg-white text-black p-2 rounded-full font-spotify text-sm">
+    <button className="bg-white text-black p-2 rounded-full font-spotify text-sm hover:scale-105 transition-all duration-300 ease-in-out">
       {children}
     </button>
   );
 }
 export function ButtonBlack({ children }) {
   return (
-    <button className="bg-black text-white p-2 rounded-full font-spotify text-sm flex items-center">
+    <button className="bg-black text-white p-2 rounded-full font-spotify text-sm flex items-center hover:scale-105 transition-all duration-300 ease-in-out">
       {children}
     </button>
   );
--- src/app/component/ui/searchbar.tsx
+++ src/app/component/ui/searchbar.tsx
@@ -2,7 +2,7 @@ import { Search, ArchiveRestore } from "lucide-react";
 
 export default function Searchbar() {
   return (
-    <div className="flex-grow max-w-96">
+    <div className="flex-grow max-w-96 hover:border-2 hover:border-white  rounded-full">
       <div className="flex items-center bg-[#121212] rounded-full px-4 py-2">
         <Search className="w-5 h-5 text-gray-400 mr-3" />
         <input
